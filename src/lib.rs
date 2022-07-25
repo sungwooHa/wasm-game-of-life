18b4a77a17950ac2475bcfe01cb333fa08186deb
sungwoo ha <hsw0312@midasit.com>
2022-07-25 18:40:59 +0900

    life of game 구조 개발

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,41 @@ extern {
 pub fn greet() {
     alert("Hello, otatop! hi");
 }
+
+#[wasm_bindgen]
+#[repr(u8)] //each cell is represented as a single byte.
+#[derive(Clone, Copy)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32, //map size
+    height: u32, //map size
+    cells: Vec<Cell>, //height * width 만큼 cell을 가짐.
+}
+ 
+impl Universe {
+    fn get_index(&self, row : u32, column : u32) -> usize{
+        (row * self.width + column) as usize
+    }
+
+    fn live_nighbor_count(&self, row: u32, column : u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height-1, 0, 1].iter().cloned() {
+            for delta_col in [self.width-1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+}
